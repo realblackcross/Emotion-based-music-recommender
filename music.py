7cb8f1b0e39e488f6d0da7c2301a7bd40b1260db
realblackcross <akarshtyagi2004@gmail.com>
2025-05-22 22:13:30 +0530

    Initial commit

---
+++ music.py
@@ -0,0 +1,230 @@
+
+# import streamlit as st
+# from streamlit_webrtc import webrtc_streamer
+# import av
+# import cv2 
+# import numpy as np 
+# import mediapipe as mp 
+# from keras.models import load_model
+# import webbrowser
+
+# model  = load_model("model.h5")
+# label = np.load("labels.npy")
+# holistic = mp.solutions.holistic
+# hands = mp.solutions.hands
+# holis = holistic.Holistic()
+# drawing = mp.solutions.drawing_utils
+
+# st.header("Emotion Based Music Recommender")
+
+# if "run" not in st.session_state:
+# 	st.session_state["run"] = "true"
+
+# try:
+# 	emotion = np.load("emotion.npy")[0]
+# except:
+# 	emotion=""
+
+# if not(emotion):
+# 	st.session_state["run"] = "true"
+# else:
+# 	st.session_state["run"] = "false"
+
+# class EmotionProcessor:
+# 	def recv(self, frame):
+# 		frm = frame.to_ndarray(format="bgr24")
+
+# 		##############################
+# 		frm = cv2.flip(frm, 1)
+
+# 		res = holis.process(cv2.cvtColor(frm, cv2.COLOR_BGR2RGB))
+
+# 		lst = []
+
+# 		if res.face_landmarks:
+# 			for i in res.face_landmarks.landmark:
+# 				lst.append(i.x - res.face_landmarks.landmark[1].x)
+# 				lst.append(i.y - res.face_landmarks.landmark[1].y)
+
+# 			if res.left_hand_landmarks:
+# 				for i in res.left_hand_landmarks.landmark:
+# 					lst.append(i.x - res.left_hand_landmarks.landmark[8].x)
+# 					lst.append(i.y - res.left_hand_landmarks.landmark[8].y)
+# 			else:
+# 				for i in range(42):
+# 					lst.append(0.0)
+
+# 			if res.right_hand_landmarks:
+# 				for i in res.right_hand_landmarks.landmark:
+# 					lst.append(i.x - res.right_hand_landmarks.landmark[8].x)
+# 					lst.append(i.y - res.right_hand_landmarks.landmark[8].y)
+# 			else:
+# 				for i in range(42):
+# 					lst.append(0.0)
+
+# 			lst = np.array(lst).reshape(1,-1)
+
+# 			pred = label[np.argmax(model.predict(lst))]
+
+# 			print(pred)
+# 			cv2.putText(frm, pred, (50,50),cv2.FONT_ITALIC, 1, (255,0,0),2)
+
+# 			np.save("emotion.npy", np.array([pred]))
+
+			
+# 		drawing.draw_landmarks(frm, res.face_landmarks, holistic.FACEMESH_TESSELATION,
+# 								landmark_drawing_spec=drawing.DrawingSpec(color=(0,0,255), thickness=-1, circle_radius=1),
+# 								connection_drawing_spec=drawing.DrawingSpec(thickness=1))
+# 		drawing.draw_landmarks(frm, res.left_hand_landmarks, hands.HAND_CONNECTIONS)
+# 		drawing.draw_landmarks(frm, res.right_hand_landmarks, hands.HAND_CONNECTIONS)
+
+
+# 		##############################
+
+# 		return av.VideoFrame.from_ndarray(frm, format="bgr24")
+
+# lang = st.text_input("Language")
+# singer = st.text_input("singer")
+
+# if lang and singer and st.session_state["run"] != "false":
+# 	webrtc_streamer(key="key", desired_playing_state=True,
+# 				video_processor_factory=EmotionProcessor)
+
+# btn = st.button("Recommend me songs")
+
+# if btn:
+# 	if not(emotion):
+# 		st.warning("Please let me capture your emotion first")
+# 		st.session_state["run"] = "true"
+# 	else:
+# 		webbrowser.open(f"https://www.youtube.com/results?search_query={lang}+{emotion}+song+{singer}")
+# 		np.save("emotion.npy", np.array([""]))
+# 		st.session_state["run"] = "false"
+
+
+
+import streamlit as st
+from streamlit_webrtc import webrtc_streamer
+import av
+import cv2
+import numpy as np
+import mediapipe as mp
+from keras.models import load_model
+import webbrowser
+
+# Load model and labels
+model = load_model("model.h5")
+label = np.load("labels.npy")
+
+# Mediapipe setup
+holistic = mp.solutions.holistic
+hands = mp.solutions.hands
+holis = holistic.Holistic()
+drawing = mp.solutions.drawing_utils
+
+# Streamlit header
+st.header("Emotion Based Music Recommender")
+
+# Initialize session state
+if "run" not in st.session_state:
+    st.session_state["run"] = "true"
+
+# Load previously detected emotion if available
+try:
+    emotion = np.load("emotion.npy")[0]
+except:
+    emotion = ""
+
+# Control the running state
+if not emotion:
+    st.session_state["run"] = "true"
+else:
+    st.session_state["run"] = "false"
+
+# Emotion processing class
+class EmotionProcessor:
+    def recv(self, frame):
+        frm = frame.to_ndarray(format="bgr24")
+
+        # Flip the frame horizontally for a mirror-like effect
+        frm = cv2.flip(frm, 1)
+
+        # Process frame with Mediapipe Holistic model
+        res = holis.process(cv2.cvtColor(frm, cv2.COLOR_BGR2RGB))
+        lst = []
+
+        # Process face landmarks
+        if res.face_landmarks:
+            for i in res.face_landmarks.landmark:
+                lst.append(i.x - res.face_landmarks.landmark[1].x)
+                lst.append(i.y - res.face_landmarks.landmark[1].y)
+
+            # Process left hand landmarks
+            if res.left_hand_landmarks:
+                for i in res.left_hand_landmarks.landmark:
+                    lst.append(i.x - res.left_hand_landmarks.landmark[8].x)
+                    lst.append(i.y - res.left_hand_landmarks.landmark[8].y)
+            else:
+                lst.extend([0.0] * 42)
+
+            # Process right hand landmarks
+            if res.right_hand_landmarks:
+                for i in res.right_hand_landmarks.landmark:
+                    lst.append(i.x - res.right_hand_landmarks.landmark[8].x)
+                    lst.append(i.y - res.right_hand_landmarks.landmark[8].y)
+            else:
+                lst.extend([0.0] * 42)
+
+            # Convert list to NumPy array and predict emotion
+            lst = np.array(lst).reshape(1, -1)
+            pred = label[np.argmax(model.predict(lst))]
+
+            # Display the detected emotion on the frame
+            cv2.putText(frm, pred, (50, 50), cv2.FONT_ITALIC, 1, (255, 0, 0), 2)
+
+            # Save detected emotion
+            np.save("emotion.npy", np.array([pred]))
+
+        # Draw face and hand landmarks
+        drawing.draw_landmarks(frm, res.face_landmarks, holistic.FACEMESH_TESSELATION,
+                               landmark_drawing_spec=drawing.DrawingSpec(color=(0, 0, 255), thickness=-1, circle_radius=1),
+                               connection_drawing_spec=drawing.DrawingSpec(thickness=1))
+        drawing.draw_landmarks(frm, res.left_hand_landmarks, hands.HAND_CONNECTIONS)
+        drawing.draw_landmarks(frm, res.right_hand_landmarks, hands.HAND_CONNECTIONS)
+
+        return av.VideoFrame.from_ndarray(frm, format="bgr24")
+
+# Input fields
+lang = st.text_input("Language")
+singer = st.text_input("Singer (Optional)")
+
+# Start video streaming if not previously detected emotion
+if lang and st.session_state["run"] != "false":
+    webrtc_streamer(key="key", desired_playing_state=True,
+                    video_processor_factory=EmotionProcessor)
+
+# Recommend button logic
+btn = st.button("Recommend me songs")
+
+if btn:
+    if not emotion:
+        st.warning("Please let me capture your emotion first.")
+        st.session_state["run"] = "true"
+    else:
+        # Build YouTube search query based on inputs
+        query = f"{lang}+{emotion}+song"
+        if singer:
+            query += f"+{singer}"
+        
+        # Open the YouTube search with the query
+        webbrowser.open(f"https://www.youtube.com/results?search_query={query}")
+        
+        # Reset emotion after recommendation
+        np.save("emotion.npy", np.array([""]))
+        st.session_state["run"] = "false"
+
+
+
+
+
+
